d99841b21e28d6fc4e66273a83bd8f1077ff3cec
demosss <spbdemosss@gmail.com>
2023-01-15 21:35:59 +0300

    end lesson 4

--- app/src/main/java/com/spbdemosss/myapplication/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/spbdemosss/myapplication/data/ShopListRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.spbdemosss.myapplication.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.spbdemosss.myapplication.domain.ShopItem
 import com.spbdemosss.myapplication.domain.ShopListRepository
 
 object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
 
     private val shopList = mutableListOf<ShopItem>()
 
@@ -22,10 +26,12 @@ object ShopListRepositoryImpl: ShopListRepository {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -40,7 +46,11 @@ object ShopListRepositoryImpl: ShopListRepository {
         } ?:  throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/spbdemosss/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/spbdemosss/myapplication/presentation/MainActivity.kt
@@ -7,14 +7,22 @@ import androidx.lifecycle.ViewModelProvider
 import com.spbdemosss.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewModel: MainViewModel
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
             Log.d("MainAct", it.toString())
+            if (count == 0){
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
